e65350f52fbdcfda73a7ca577e87ef2ed704225e
yamao-sys <yamaopanku@gmail.com>
2024-10-05 14:46:33 +0900

    time

---
+++ app/time/main.go
@@ -0,0 +1,56 @@
+package main
+
+func main() {
+	// 時間の生成
+	// t := time.Now()
+	// fmt.Println(t)
+
+	// t2 := time.Date(2024, 10, 5, 14, 31, 0, 0, time.Local)
+	// fmt.Println(t2)
+	// fmt.Println(t2.Year())
+	// fmt.Println(t2.YearDay())
+	// fmt.Println(t2.Month())
+	// fmt.Println(t2.Weekday())
+	// fmt.Println(t2.Day())
+	// fmt.Println(t2.Hour())
+	// fmt.Println(t2.Minute())
+	// fmt.Println(t2.Second())
+	// fmt.Println(t2.Nanosecond())
+	// fmt.Println(t2.Zone())
+
+	// time.Duration型を返す
+	// fmt.Println(time.Hour)
+	// fmt.Printf("%T\n", time.Hour)
+	// fmt.Println(time.Minute)
+	// fmt.Println(time.Second)
+	// fmt.Println(time.Millisecond)
+	// fmt.Println(time.Microsecond)
+	// fmt.Println(time.Nanosecond)
+
+	// time.Duration型を文字列から生成する
+	// d, _ := time.ParseDuration("2h30m")
+	// fmt.Println(d)
+
+	// t3 := time.Now()
+	// t3 = t3.Add(2*time.Minute + 15*time.Second)
+	// fmt.Println(t3)
+
+	// 時間の差分の取得
+	// t5 := time.Date(2023, 10, 5, 0, 0, 0, 0, time.Local)
+	// t6 := time.Now()
+	// fmt.Println(t6)
+
+	// t5 - t6
+	// d2 := t5.Sub(t6)
+	// fmt.Println(d2)
+
+	// 時刻を比較する
+	// fmt.Println(t6.Before(t5))
+	// fmt.Println(t6.After(t5))
+	// fmt.Println(t5.Before(t6))
+	// fmt.Println(t5.After(t6))
+
+	// 指定時間のSleep
+	// time.Sleep(5 * time.Second)
+	// fmt.Println("5秒停止後表示")
+}
