5b37400011cf8c5704f45915773c8043c8d6c950
yamao-sys <yamaopanku@gmail.com>
2024-10-08 10:01:56 +0900

    net/http server

---
+++ app/net_http_server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type MyHandler struct{}
+
+// func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello World!")
+// }
+
+func top(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("tmpl.html")
+	if err != nil {
+		log.Println(err)
+	}
+	t.Execute(w, "Hello World!!!!")
+}
+
+func main() {
+	// http.ListenAndServe(":8080", &MyHandler{})
+	http.HandleFunc("/top", top)
+	http.ListenAndServe(":8080", nil)
+}
